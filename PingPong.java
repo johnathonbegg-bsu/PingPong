f69d324490ad4e3f7c5e4e32117d70b8e6e22c37
johnathonbegg-bsu <43049002+johnathonbegg-bsu@users.noreply.github.com>
2020-02-14 00:00:24 -0700

    Add files via upload
    Server is now multi-threaded and can handle multiple users.
    Claims to have maximum number of clients, but is not enforced by software . Will modify feature. 
    uses a depreciated mothod utilizing Integer Objects send over the socket streams. Has not impeded function of program, but will look into alternitives. 
    Also tracks number of games.

--- PingPong.java
+++ PingPong.java
@@ -3,93 +3,107 @@ import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Random;
 
-public class PingPong {
+public class PingPong extends Thread {
 
-	private InputStream in;
 	private ObjectInputStream oin;
-	private OutputStream out;
 	private ObjectOutputStream oout;
-	private ServerSocket serverSock;
+	@SuppressWarnings("unused")
 	private Socket sock;
-	private int numConnectedClients;
-	private int maxConnections;
+	private static int numConnectedClients;
+	private static int maxConnections =10;
 	private Random rand = new Random();
 	private boolean sender;
 	private boolean running = true;
+	private int delay = 500;
+	private int gameID;
 
 	public static void main(String[] args) {
 		System.out.println("java PingPong <client|server> <serverHost> <port#>");
 		String host = args[1];
 		int port = Integer.parseInt(args[2]);
 
+		ServerSocket serverSock;
+
 		if (args[0].equals("server")) {
 			System.out.println("attempting to create server");
-			PingPong myServer = new PingPong(port);
-			myServer.serverConnect();
+			try {
+				serverSock = new ServerSocket(port);
+				System.out.println("PingPong gerver up and running on port:" + port);
+
+				Socket sock = null;
+				while (true) {
+					if( numConnectedClients >= maxConnections){
+						System.out.println("max connections reached.");
+					}
+					sock = serverSock.accept();
+					numConnectedClients +=1;
+					
+					Thread myServer = new PingPong(sock);
+					((PingPong)myServer).ServerDoCoinToss();
+					myServer.start();
+				}
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		} else if (args[0].equals("client")) {
-			System.out.println("attempting to connect to server");
-			PingPong myClient = new PingPong(host, port);
-			myClient.clientPlayPingPong();
+
+			try {
+				System.out.println("attempting to connect to server");
+				System.out.println("establishing connection");
+				Socket sock = new Socket(host, port);
+
+				Thread myClient = new PingPong(sock);
+
+				((PingPong) myClient).ClientDoCoinToss();
+				myClient.start();
+
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 		}
 
 	}
 
-	/**
-	 * Build Server
-	 * 
-	 * @param port
-	 */
-	public PingPong(int port) {
+	public PingPong(Socket sock) {
+		this.sock = sock;
 		try {
-			serverSock = new ServerSocket(port);
-			System.out.println("PingPong gerver up and running on port:" + port);
-			numConnectedClients = 0;
-			// limit number of threads
-			maxConnections = 1;
-			
+			this.oout = new ObjectOutputStream(sock.getOutputStream());
+			this.oin = new ObjectInputStream(sock.getInputStream());
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			System.out.println("failed to establish server at port:" + port);
 			e.printStackTrace();
 		}
+
 	}
 
-	/**
-	 * Build
-	 * 
-	 * @param host
-	 * @param port
-	 */
-	public PingPong(String host, int port) {
-		// CLIENT
+	public synchronized void ClientDoCoinToss() {
+
 		try {
-			System.out.println("establishing connection");
-			sock = new Socket(host, port);
-			out = sock.getOutputStream();
-			in = sock.getInputStream();
-
-			oout = new ObjectOutputStream(out);
-			oin = new ObjectInputStream(in);
-
-			// COIN TOSS
-			Integer clientCoin = new Integer(rand.nextInt());
-			oout.writeObject(clientCoin);
-			Integer serverCoin = (Integer) oin.readObject();
-
-			if (serverCoin.compareTo(clientCoin) >= 0)
-				sender = false;
-			else
+			Integer cint = new Integer(rand.nextInt());
+			Integer sint = (Integer)oin.readObject();
+			oout.writeObject(cint);
+			Integer gID = (Integer)oin.readObject();
+			gameID = gID.intValue();
+					
+			if (sint.compareTo(cint) < 0)
 				sender = true;
-
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			else
+				sender = false;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -97,88 +111,57 @@ public class PingPong {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
 
-	public void serverConnect() {
-		Socket sock;
-		System.out.println("waiting for clients");
-		while (true) {
-			try {
-				// if there is still room to connect
-				// then accept the connection
-				if (numConnectedClients < maxConnections) {
-					sock = serverSock.accept();
-					numConnectedClients++;
-
-					out = sock.getOutputStream();
-					in = sock.getInputStream();
-
-					oout = new ObjectOutputStream(out);
-					oin = new ObjectInputStream(in);
-
-					// DO COIN TOSS
-					Integer clientCoin = (Integer) oin.readObject();
-					Integer serverCoin = new Integer(rand.nextInt());
-					oout.writeObject(serverCoin);
-
-					if (serverCoin.compareTo(clientCoin) >= 0)
-						sender = true;
-					else
-						sender = false;
-
-					serverPlayPingPong(sock);
-					//running = false;
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-
-	private void serverPlayPingPong(Socket sock) throws IOException, ClassNotFoundException {
-		while (running){
-			if(sender){
-				Ball ball = new Ball();
-				oout.writeObject(ball);
-				System.out.println("ping");
-				
-				
-			}else{
-				Ball ball = (Ball) oin.readObject();
-				//System.out.println("ping");
-
-				oout.writeObject(new Ball());
-				System.out.println("pong");
-			}
+	public synchronized void ServerDoCoinToss() {
+		try {
 			
-		}
-		
+			Integer sint = new Integer(rand.nextInt());
+			gameID = numConnectedClients;
+			Integer gID = new Integer(gameID);
+			oout.writeObject(sint);
+			Integer cint = (Integer)oin.readObject();
+			oout.writeObject(gID);
 
+			if (sint.compareTo(cint) >=0)
+				sender = true;
+			else
+				sender = false;
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
-	// CLIENT CODE
+	public void run() {
+		while (true) {
+			PlayPingPong();
+		}
+	}
 
-	public void clientPlayPingPong() {
-		// TODO Auto-generated method stub
+	public void PlayPingPong() {
 		while (running) {
 			try {
 
-				if(sender){
+				if (sender) {
+					// send object
+					Thread.sleep(delay);
 					Ball ball = new Ball();
 					oout.writeObject(ball);
-					System.out.println("ping");
-					
-					
-				}else{
-					Ball ball = (Ball) oin.readObject();
-					//System.out.println("ping");
+					System.out.println("ping game: " + gameID);
 
+					// recieve object
+					ball = (Ball) oin.readObject();
+
+				} else {
+					// recieve object
+					Ball ball = (Ball) oin.readObject();
+
+					// send object
+					Thread.sleep(delay);
 					oout.writeObject(new Ball());
-					System.out.println("pong");
+					System.out.println("pong game: " + gameID );
 				}
 
 				// sock.close();
@@ -188,8 +171,12 @@ public class PingPong {
 			} catch (ClassNotFoundException e) {
 
 				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
+
 	}
 
 }
